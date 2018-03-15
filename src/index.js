5d2bf6dd7b673e9a4828544dc793b6ad6553ae92
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-16 04:22:21 +0700

    Add text to node in index.js

--- src/index.js
+++ src/index.js
@@ -8,39 +8,36 @@ const g = svg.append('g').attr('class', 'g_root');
 function newNode(text) {
   const rd = {
     ri: {
-      w: 20,
-      h: 20
+      w: 22,
+      h: 34
     },
     rc: {
       w: 120,
-      h: 30
+      h: 36
     },
     ro: {
-      w: 20,
-      h: 20
+      w: 22,
+      h: 34
     }
   };
 
   const node = g
     .append('g')
     .attr('class', 'node')
+    .style('cursor', 'pointer')
     .call(d3.drag().on('drag', handleDragged));
 
   function handleDragged() {
     const rectInput = d3.select(this).select('.rectInput'),
       rectContent = d3.select(this).select('.rectContent'),
+      rectText = d3.select(this).select('.rectText'),
       rectOutput = d3.select(this).select('.rectOutput'),
       x = d3.event.x,
       y = d3.event.y;
-    rectInput
-      .attr('x', x - rd.rc.w / 2 - rd.ri.w)
-      .attr('y', y - rd.ri.h / 2);
-    rectContent
-      .attr('x', x - rd.rc.w / 2)
-      .attr('y', y - rd.rc.h / 2);
-    rectOutput
-      .attr('x', x + rd.rc.w / 2)
-      .attr('y', y - rd.ro.h / 2);
+    rectInput.attr('x', x - rd.rc.w / 2 - rd.ri.w).attr('y', y - rd.ri.h / 2);
+    rectContent.attr('x', x - rd.rc.w / 2).attr('y', y - rd.rc.h / 2);
+    rectText.attr('x', x).attr('y', y);
+    rectOutput.attr('x', x + rd.rc.w / 2).attr('y', y - rd.ro.h / 2);
   }
 
   const rectInput = node
@@ -62,6 +59,15 @@ function newNode(text) {
     .attr('y', 0)
     .attr('fill', 'rgb(255, 252, 218)')
     .attr('stroke', 'rgb(117, 107, 0)');
+
+  const rectText = node
+    .append('text')
+    .attr('class', 'rectText')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+    .attr('x', rd.ri.w + rd.rc.w / 2)
+    .attr('y', rd.rc.h / 2)
+    .text(text);
 
   const rectOutput = node
     .append('rect')
